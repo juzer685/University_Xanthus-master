6b2c790225a81e4f6ad1a50bdf4c53bab5b1b46a
prathameshg@xanthus.in <prathameshg@xanthus.in>
2020-03-16 19:49:33 +0530

    Change Multiple Add AddCard,Home Page html Changes, Admin Add video display

--- University.UI/Scripts/jsCardList.js
+++ University.UI/Scripts/jsCardList.js
@@ -5,51 +5,53 @@
 
 function RegisterHandler() {
 
-    $('.btnPaymentGateway').click(function ()
-    {
-        var Obj = {
-            //DBCardNumber: $('.CardNumber').html(),
-            Amount: parseFloat($('.amount').html()),
-            CVV: $(this).prev().find(".datacvv").val(),
-            CardNumber: $(this).parent().parent().parent().find('.dataCardNumber').html(),
-            isProductbuy: $("input[name='radioCardNumber']:checked").siblings('.isProductbuy').val(),
-            isbuy: $("input[name='radioCardNumber']:checked").siblings('.isbuy').val(),
-            VideoId: $("input[name='radioCardNumber']:checked").siblings('.VideoId').val(),
-            ProductId: $("input[name='radioCardNumber']:checked").siblings('.ProductId').val(),
-            SubCatID: $("input[name='radioCardNumber']:checked").siblings('.SubCatID').val(),
-            createby: $("input[name='radioCardNumber']:checked").siblings('.createby').val(),
-            ProductName: $("input[name='radioCardNumber']:checked").siblings('.ProductName').val(),
-            CustomerFName: $("input[name='radioCardNumber']:checked").siblings('.CustomerFName').val(),
-            CardHolderName: $("input[name='radioCardNumber']:checked").siblings('.CardHolderName').val(),
-            Month: $("input[name='radioCardNumber']:checked").siblings('.Month').val(),
-            Year: $("input[name='radioCardNumber']:checked").siblings('.Year').val(),
-            CustomerProfileId: $("input[name='radioCardNumber']:checked").siblings('.CustomerProfileId').val(),
-            CustomerPaymentProfileId: $("input[name='radioCardNumber']:checked").siblings('.CustomerPaymentProfileId').val(),
+    $('.btnPaymentGateway').click(function () {
+       
 
-        };
-        //document.getElementById('loaderring').style.display = "block";
-        $.ajax({
-            type: "POST",
-            url: "/PaymentGateway/MakePaymentUsingProfile",
-            data: Obj,
-            success: function (data) {
-                if (data.result == true) {
-                    _showSuccessMessage(data.Message);
-                    //document.getElementById('loaderring').style.display = "none";
-                    setTimeout(function () { window.location.href = '/Home/Index'; }, 2000);
+            var Obj = {
+                //DBCardNumber: $('.CardNumber').html(),
+                Amount: parseFloat($('.amount').html()),
+                CVV: $(this).prev().find(".datacvv").val(),
+                CardNumber: $(this).parent().parent().parent().find('.dataCardNumber').html(),
+                isProductbuy: $("input[name='radioCardNumber']:checked").siblings('.isProductbuy').val(),
+                isbuy: $("input[name='radioCardNumber']:checked").siblings('.isbuy').val(),
+                VideoId: $("input[name='radioCardNumber']:checked").siblings('.VideoId').val(),
+                ProductId: $("input[name='radioCardNumber']:checked").siblings('.ProductId').val(),
+                SubCatID: $("input[name='radioCardNumber']:checked").siblings('.SubCatID').val(),
+                createby: $("input[name='radioCardNumber']:checked").siblings('.createby').val(),
+                ProductName: $("input[name='radioCardNumber']:checked").siblings('.ProductName').val(),
+                CustomerFName: $("input[name='radioCardNumber']:checked").siblings('.CustomerFName').val(),
+                CardHolderName: $("input[name='radioCardNumber']:checked").siblings('.CardHolderName').val(),
+                Month: $("input[name='radioCardNumber']:checked").siblings('.Month').val(),
+                Year: $("input[name='radioCardNumber']:checked").siblings('.Year').val(),
+                CustomerProfileId: $("input[name='radioCardNumber']:checked").siblings('.CustomerProfileId').val(),
+                CustomerPaymentProfileId: $("input[name='radioCardNumber']:checked").siblings('.CustomerPaymentProfileId').val(),
+
+            };
+            //document.getElementById('loaderring').style.display = "block";
+            $.ajax({
+                type: "POST",
+                url: "/PaymentGateway/MakePaymentUsingProfile",
+                data: Obj,
+                success: function (data) {
+                    if (data.result == true) {
+                        _showSuccessMessage(data.Message);
+                        //document.getElementById('loaderring').style.display = "none";
+                        setTimeout(function () { window.location.href = '/Home/Index'; }, 2000);
+                    }
+                    else {
+                        _showErrorMessage(data.Message);
+                        //document.getElementById('loaderring').style.display = "none";
+                        setTimeout(function () { window.location.href = '/Home/Index'; }, 2000);
+                    }
                 }
-                else {
-                    _showErrorMessage(data.Message);
-                    //document.getElementById('loaderring').style.display = "none";
-                    setTimeout(function () { window.location.href = '/Home/Index'; }, 2000);
-                }
-            }
+            });
+            //}
+            //else {
+            //    _showErrorMessage("Please agree to terms");
+            //}
+            //}
         });
-        //}
-        //else {
-        //    _showErrorMessage("Please agree to terms");
-        //}
-        //}
-    });
+  
 }
 
