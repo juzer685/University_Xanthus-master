22151f6499accb92c78b58c4160879cb409e84fc
prathameshg@xanthus.in <prathameshg@xanthus.in>
2020-04-07 11:37:35 +0530

    changes

--- University.UI/Areas/Admin/Models/ProductUserGuideViewModel.cs
+++ University.UI/Areas/Admin/Models/ProductUserGuideViewModel.cs
@@ -28,11 +28,11 @@ namespace University.UI.Areas.Admin.Models
         public Decimal AssocitedCustID { get; set; }
        // [StringLength(100, ErrorMessage = "Do not enter more than 100 characters")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "Please select file.")]
+       // [Required(ErrorMessage = "Please select file.")]
         public HttpPostedFileBase file { get; set; }
         public string ImageURL { get; set; }
         //[StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
-        [Required(ErrorMessage = "Please select file.")]
+        //[Required(ErrorMessage = "Please select file.")]
         public string Title { get; set; }
         public Nullable<Decimal> ProductId { get; set; }
         public Nullable<bool> IsDeleted { get; set; }
@@ -43,7 +43,7 @@ namespace University.UI.Areas.Admin.Models
         public Nullable<System.DateTime> UpdatedDate { get; set; }
         public Nullable<Decimal> UpdatedBy { get; set; }
         public HttpPostedFileBase Guidefile { get; set; }
-        [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
+       // [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
         public string DocumentURL { get; set; }
         public string ImageALT { get; set; }
         public string CheckImage
